94d826d680d4b32f6329c967ed5f2bc08a4a0df3
dave95 <davidegallitelli@gmail.com>
2015-11-06 00:20:39 +0100

    # COMMIT 06112015 # 4 #
    Built the frame for exercise 4

---
+++ Modulo 2 - Practica 1/ejercicio4.c
@@ -0,0 +1,25 @@
+//================================//
+// Created by dave95 on 05/11/15. //
+// Source Code for exercise 2     //
+// of the Sistemas Operativos     //
+// course of UGR, year 2015/16    //
+//================================//
+//      COPY WITH CAUTION!        //
+//================================//
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+//to ignore the warnings
+#include<unistd.h>
+//for working on strings, functions like strcpy() and strcat()
+#include<string.h>
+
+int main(int argc, char* argv[]){
+
+    return 0;
+
+}
