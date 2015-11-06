f59c3efa3f464db363697ad011c950fe966fed87
dave95 <davidegallitelli@gmail.com>
2015-11-06 20:58:02 +0100

    # COMMIT 06112015 # 7 #
    Completed Exercise 4 (minor modifications to the exercise 3 code)

--- Modulo 2 - Practica 1/ejercicio4.c
+++ Modulo 2 - Practica 1/ejercicio4.c
@@ -18,8 +18,39 @@
 //for working on strings, functions like strcpy() and strcat()
 #include<string.h>
 
-int main(int argc, char* argv[]){
+// Defining the macro
+// It checks on the IFMT field (filetype) if it's equal to the regular flag
+#define S_ISREG2(mode)      (((mode) & S_IFMT) == 0100000)
+
+int main(int argc, char *argv[])
+{
+    int i;
+    struct stat atributos;
+    char tipoArchivo[30];
+
+    if(argc<2) {
+        printf("\nSintaxis de ejecucion: tarea2 [<nombre_archivo>]+\n\n");
+        exit(-1);
+    }
+    for(i=1;i<argc;i++) {
+        printf("%s: ", argv[i]);
+        if(lstat(argv[i],&atributos) < 0) {
+            printf("\nError al intentar acceder a los atributos de %s",argv[i]);
+            perror("\nError en lstat");
+        }
+        else {
+            //Here we call the newly-defined macro
+            if(S_ISREG2(atributos.st_mode)) strcpy(tipoArchivo,"Regular");
+            else if(S_ISDIR(atributos.st_mode)) strcpy(tipoArchivo,"Directorio");
+            else if(S_ISCHR(atributos.st_mode)) strcpy(tipoArchivo,"Especial de caracteres");
+            else if(S_ISBLK(atributos.st_mode)) strcpy(tipoArchivo,"Especial de bloques");
+            else if(S_ISFIFO(atributos.st_mode)) strcpy(tipoArchivo,"Tubería con nombre (FIFO)");
+            else if(S_ISLNK(atributos.st_mode)) strcpy(tipoArchivo,"Enlace relativo (soft)");
+            else if(S_ISSOCK(atributos.st_mode)) strcpy(tipoArchivo,"Socket");
+            else strcpy(tipoArchivo,"Tipo de archivo desconocido");
+            printf("%s\n",tipoArchivo);
+        }
+    }
 
     return 0;
-
 }
